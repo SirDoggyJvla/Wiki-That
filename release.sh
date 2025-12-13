9330b7000d4e244d8bbcf3f8ae1d46562b810cba
SirDoggyJvla <simon.cadet79@hotmail.fr>
2025-12-13 22:23:45 +0100

    release files

--- release.sh
+++ release.sh
@@ -1,10 +1,9 @@
 # store current folder
-CURRENT_DIR=$(pwd)
+WORKSHOP_DIR=$(pwd)
 
-REPO_DIR="/home/simon/Documents/Repositories/Steam-Uploader-tool/releases/0.5.0-ubuntu"
-WORKSHOP_DIR="$(pwd)"
-
-cd "$REPO_DIR"
+# need to be in the steam uploader folder
+cd "$STEAMUPLOADER"
 ./SteamUploader -a 108600 -w 3567109091 -d "$WORKSHOP_DIR/description.bbcode" -P "$WORKSHOP_DIR/patch_note.bbcode" -c "$WORKSHOP_DIR/Contents"
 
-cd $CURRENT_DIR
+# return to original folder
+cd $WORKSHOP_DIR
