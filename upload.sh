bdf80c5a5e3275e238a5cd22af78a050b9e21f77
SimKDT <simon.cadet79@hotmail.fr>
2026-03-16 21:52:18 +0100

    cd to uploader path

--- upload.sh
+++ upload.sh
@@ -10,4 +10,5 @@ WORKSHOP_ID=3567109091
 VISIBILITY=0
 TAGS="Build 42,Interface,Literature,Misc,QoL"
 
-$STEAMUPLOADER/SteamUploader --appID 108600 --workshopID "$WORKSHOP_ID" --description "$WORKSHOP_DIR/description.bbcode" --patchNote "$WORKSHOP_DIR/patch_note.bbcode" -c "$WORKSHOP_DIR/Contents" --preview "$WORKSHOP_DIR/Wiki That! - preview.png" --title "$MOD_TITLE" --visibility "$VISIBILITY" --tags "$TAGS"
+cd $STEAMUPLOADER
+./SteamUploader --appID 108600 --workshopID "$WORKSHOP_ID" --description "$WORKSHOP_DIR/description.bbcode" --patchNote "$WORKSHOP_DIR/patch_note.bbcode" -c "$WORKSHOP_DIR/Contents" --preview "$WORKSHOP_DIR/Wiki That! - preview.png" --title "$MOD_TITLE" --visibility "$VISIBILITY" --tags "$TAGS"
