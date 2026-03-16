157dd017fc9b363962c601f4c6a43787d2989426
SimKDT <simon.cadet79@hotmail.fr>
2026-03-16 21:57:36 +0100

    GitHub release for the mod

--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
+set -e
+
 # make sure main branch is being uploaded
 git checkout main
 git pull
@@ -11,4 +13,20 @@ VISIBILITY=0
 TAGS="Build 42,Interface,Literature,Misc,QoL"
 
 cd $STEAMUPLOADER
-./SteamUploader --appID 108600 --workshopID "$WORKSHOP_ID" --description "$WORKSHOP_DIR/description.bbcode" --patchNote "$WORKSHOP_DIR/patch_note.bbcode" -c "$WORKSHOP_DIR/Contents" --preview "$WORKSHOP_DIR/Wiki That! - preview.png" --title "$MOD_TITLE" --visibility "$VISIBILITY" --tags "$TAGS"
+./SteamUploader --appID 108600 --workshopID "$WORKSHOP_ID" \
+    --description "$WORKSHOP_DIR/description.bbcode" \
+    --patchNote "$WORKSHOP_DIR/steam_description/patch_notes/$VERSION.bbcode" \
+    -c "$WORKSHOP_DIR/Contents" \
+    --preview "$WORKSHOP_DIR/Wiki That! - preview.png" \
+    --title "$MOD_TITLE" --visibility "$VISIBILITY" --tags "$TAGS"
+
+cd $WORKSHOP_DIR
+
+ARCHIVE="/tmp/release.zip"
+zip -r "$ARCHIVE" Contents/mods
+
+gh release create "$VERSION" "$ARCHIVE" \
+    --notes "$VERSION" \
+    "$@"
+
+rm -f "$ARCHIVE"
